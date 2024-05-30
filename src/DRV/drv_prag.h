c8df090db11c5bcab45c66b478eb6cdbc76d8794
TIMA <64804931+turatsamar@users.noreply.github.com>
2024-05-31 00:53:19 +0900

    Update drv_prag.h

--- src/DRV/drv_prag.h
+++ src/DRV/drv_prag.h
@@ -1,13 +1,13 @@
 /*********************************************************************************************************************/
-/*’˜ìŠ—LŒ  F¬“‡ƒvƒŒƒXH‹Æ(Š”) “dŽq‹Zp•” “dŽqÝŒv‰Û                                                              */
-/*ƒe[ƒ}–¼   FSTEP3@@@@@@@@@@                                                                              */
-/*ƒ[ƒJ     Fƒ‹ƒlƒTƒXƒGƒŒƒNƒgƒƒjƒNƒX                                                                              */
-/*ƒfƒoƒCƒX   FR5F10PPJ(RL78/F13)                                                                                    */
-/*ƒtƒ@ƒCƒ‹–¼ Fdrv_pragma.h                                                                                                */
-/*Žd—l       F”Ä—pƒvƒ‰ƒOƒ}éŒ¾                                                                                      */
+/*è‘—ä½œæ‰€æœ‰æ¨© ï¼š                                                              */
+/*ãƒ†ãƒ¼ãƒžå   ï¼šSTEP3ã€€ã€€ã€€ã€€ã€€ã€€ã€€ã€€ã€€ã€€                                                                              */
+/*ãƒ¡ãƒ¼ã‚«     ï¼šãƒ«ãƒã‚µã‚¹ã‚¨ãƒ¬ã‚¯ãƒˆãƒ­ãƒ‹ã‚¯ã‚¹                                                                              */
+/*ãƒ‡ãƒã‚¤ã‚¹   ï¼šR5F10PPJ(RL78/F13)                                                                                    */
+/*ãƒ•ã‚¡ã‚¤ãƒ«å ï¼šdrv_pragma.h                                                                                                */
+/*ä»•æ§˜       ï¼šæ±Žç”¨ãƒ—ãƒ©ã‚°ãƒžå®£è¨€                                                                                      */
 /*********************************************************************************************************************/
-/*—š—ð                                                                                                               */
-/* 2024.**.**FV‹Kì¬                                                                                   ƒgƒ‰ƒxƒbƒN */
+/*å±¥æ­´                                                                                                               */
+/* 2024.**.**ï¼šæ–°è¦ä½œæˆ                                                                                   ãƒˆãƒ©ãƒ™ãƒƒã‚¯ */
 /*                                                                                                                   */
 /*                                                                                                                   */
 /*                                                                                                                   */
@@ -26,12 +26,12 @@
 #include "../rl78_ca78k0r.h"
 
 
-#include "./drv_mac.h"        /* ƒ}ƒNƒ’è‹`ƒtƒ@ƒCƒ‹ */
-#include "./drv_type.h"       /* Œ^’è‹`ƒtƒ@ƒCƒ‹ */
-#include "./drv_glob.h"       /* ƒOƒ[ƒoƒ‹•Ï”éŒ¾ƒtƒ@ƒCƒ‹ */
-#include "./drv_prot.h"       /* ŠÖ”ƒvƒƒgƒ^ƒCƒvéŒ¾ƒtƒ@ƒCƒ‹ */
-#include "./drv_cnst.h"       /* const•Ï”éŒ¾ƒtƒ@ƒCƒ‹ */
-#include "./drv_stat.h"       /* static•Ï”AŠÖ”ƒvƒƒgƒ^ƒCƒvéŒ¾ƒtƒ@ƒCƒ‹ */
+#include "./drv_mac.h"        /* ãƒžã‚¯ãƒ­å®šç¾©ãƒ•ã‚¡ã‚¤ãƒ« */
+#include "./drv_type.h"       /* åž‹å®šç¾©ãƒ•ã‚¡ã‚¤ãƒ« */
+#include "./drv_glob.h"       /* ã‚°ãƒ­ãƒ¼ãƒãƒ«å¤‰æ•°å®£è¨€ãƒ•ã‚¡ã‚¤ãƒ« */
+#include "./drv_prot.h"       /* é–¢æ•°ãƒ—ãƒ­ãƒˆã‚¿ã‚¤ãƒ—å®£è¨€ãƒ•ã‚¡ã‚¤ãƒ« */
+#include "./drv_cnst.h"       /* constå¤‰æ•°å®£è¨€ãƒ•ã‚¡ã‚¤ãƒ« */
+#include "./drv_stat.h"       /* staticå¤‰æ•°ã€é–¢æ•°ãƒ—ãƒ­ãƒˆã‚¿ã‚¤ãƒ—å®£è¨€ãƒ•ã‚¡ã‚¤ãƒ« */
 
 
 
